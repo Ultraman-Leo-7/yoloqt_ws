0ddc3ed1273d8fedfca43cef01b35de06e9ff16d
leo3407 <3540755883@qq.com>
2026-01-18 15:08:15 +0800

    由于在qt打开带来的影响

---
+++ build-src-Desktop_Qt_5_12_12_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -0,0 +1,8 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/software/Qt/Qt5.12.12/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "C:/software/Qt/Qt5.12.12/5.12.12/mingw73_64/bin/qmake.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "C:/software/Qt/Qt5.12.12/5.12.12/mingw73_64" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/software/Qt/Qt5.12.12/Tools/mingw730_64/bin/gcc.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/software/Qt/Qt5.12.12/Tools/mingw730_64/bin/g++.exe" CACHE "STRING" "" FORCE)
